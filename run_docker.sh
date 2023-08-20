6240e71778d77fc5063b851c2eadcac3ff14a051
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2023-08-21 00:22:01 +0200

    fixed command

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,3 @@
 mkdir outputs
 docker build . -t MXMNetDocker:v1.0
-docker run MXMNetDocker:v1.0 -v ./outputs:/MXMNet/outputs
-python -u main.py --target 2 --cutoff 10 --batch_size 128 --excluded_mols_file data/excludedMolecules_MG.txt > outputs/docker_output.txt
+docker run MXMNetDocker:v1.0 -v ./outputs:/MXMNet/outputs python -u main.py --target 2 --cutoff 10 --batch_size 128 --excluded_mols_file data/excludedMolecules_MG.txt > outputs/docker_output.txt
